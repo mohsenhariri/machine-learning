9fe67cea938f185d6dfbd4c31cd9ac027a818123
Mohsen Hariri <mhariri68@gmail.com>
2021-12-15 20:23:42 -0500

    mnist cnn model

---
+++ mnist/data_prep.py
@@ -0,0 +1,20 @@
+from os import path, environ
+from torchvision import datasets, transforms
+from torch.utils import data
+
+batch_size = int(environ.get("BATCH_SIZE"))
+
+transform = transforms.ToTensor()
+file_exists = path.exists("./data/MNIST")
+
+train_data = datasets.MNIST(root="./data", train=True, transform=transform, download=not file_exists)
+# print(train_data)
+### Number of datapoints: 60000
+
+test_data = datasets.MNIST(root="./data", transform=transform, train=False)
+# print(test_data)
+### Number of datapoints: 10000
+
+train_loader = data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+# test_loader = data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
+test_loader = data.DataLoader(dataset=test_data)
---
+++ mnist/model.py
@@ -0,0 +1,92 @@
+from os import environ
+import time
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from data_prep import train_loader, test_loader
+
+# Hyperparamethers
+num_epochs = int(environ.get("NUM_EPOCHS"))
+lr = float(environ.get("LEARNING_RATE"))
+
+
+class CNN(nn.Module):
+    def __init__(self) -> None:
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.fc1 = nn.Linear(in_features=32 * 7 * 7, out_features=10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        out = self.fc1(x)
+        return out, x
+
+
+model = CNN()
+# print(model)
+
+loss_func = nn.CrossEntropyLoss()
+
+# optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+
+
+for i in range(num_epochs):
+    for j, (x, y) in enumerate(train_loader):
+        # x and y includes batch_size samples
+        y_estimated_hat = model(x)[0]
+        y_real = y
+        loss = loss_func(model(x)[0], y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(f"Epoch: {i+1}, loss after {j+1} bach {loss_func(y_estimated_hat,y_real)}")
+
+
+print("Training was finished.")
+current_time = time.strftime("%H-%M-%S")
+path = f"./mnist/model/{current_time}.model"
+torch.save(model.state_dict(), path)
+
+
+def show(img, y_estimate, y_label):
+    plt.imshow(img, cmap="gray")
+    plt.title(f"Estimate: {y_estimate}, Label: {y_label}")
+    plt.show()
+
+
+f = 0
+for i, (x, y) in enumerate(test_loader):
+    model_output = model(x)[0]
+    y_hat = torch.argmax(model_output)
+    if y_hat.item() != y.item():
+        show(x[0][0], y_hat.item(), y.item())
+        f = f + 1
+
+print(f"Total number of false estimation : {f}")
+print(f"Percent: {100- (100*f)/(i+1)}")
