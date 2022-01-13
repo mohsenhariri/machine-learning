70c3ca829db745b500d42373ec803dba5931818a
Mohsen Hariri <mhariri68@gmail.com>
2022-01-12 19:00:05 -0500

    model initialization added

--- image_processing/semantic_segmentation.py
+++ image_processing/semantic_segmentation.py
@@ -15,25 +15,22 @@ while True:
     file_exists = path.exists(f"./image_processing/runs/{j}")
     if not file_exists:
         break
-
 writer = SummaryWriter(log_dir=f"./image_processing/runs/{j}")
 
 
-# t23 = torch.tensor([[1, 0], [0, 1]]).reshape([1, 1, 2, 2])
-# t23 = torch.tensor([[1, 0], [0, 1]]).reshape([1, 1, 2, 2])
+kernel = [[-1.0, -1.0, -1.0], [-1.0, 8.0, -1.0], [-1.0, -1.0, -1.0]]
+emboss_kernel = [[-2.0, -1.0, 0.0], [-1.0, 1.0, 1.0], [0.0, 1.0, 2.0]]
+sobel_kernel = [[-1.0, 0.0, 1.0], [-2.0, 0.0, 2.0], [-1.0, 0.0, 1.0]]
 
-kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-# sobel_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
 initial_value = torch.tensor(kernel)
 
 
 class NN(nn.Module):
     def __init__(self) -> None:
         super(NN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3)
-        # nn.init.kaiming_normal_(self.conv1.weight, mode="fan_in", nonlinearity="relu")
-        # nn.init.kaiming_normal_(self.fc2.weight, mode="fan_in", nonlinearity="relu")
-        # nn.init.xavier_normal_(self.fc3.weight)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
+
         with torch.no_grad():
             self.conv1.weight.copy_(initial_value)
 
@@ -41,38 +38,25 @@ class NN(nn.Module):
         out = self.conv1(x)
         return out
 
-    def help(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                pass
-                # nn.init.kaiming_uniform_(m.weight)
-                # nn.init.constant_(m.weight, 0)
-                # nn.init.(m.weight, 0)
-                # m.weight = nn.parameter(t23)
-
-                # if m.bias is not None:
-                #     print("ok")
-                #     nn.init.constant_(m.bias, 0)
-
-                # print(m.weight, m.bias)
-                # print(m.weight.size())
-        pass
-
 
 model = NN()
 
 
-model.help()
-#
-
-# print(t23.size())
 for batch_ndx, (x, y) in enumerate(train_loader):
     img_grid = torchvision.utils.make_grid(x)
     writer.add_image(tag=f"Original- batch_iter: {batch_ndx}", img_tensor=img_grid)
 
     output_conv = model(x)
-    img_grid = torchvision.utils.make_grid(output_conv)
-    writer.add_image(tag=f"batch_iter: {batch_ndx}", img_tensor=img_grid)
+    print(output_conv.size())
+
+    img_grid0 = torchvision.utils.make_grid(output_conv[0:, 0, :, :])
+    img_grid1 = torchvision.utils.make_grid(output_conv[0:, 1, :, :])
+    img_grid2 = torchvision.utils.make_grid(output_conv[0:, 2, :, :])
+
+    writer.add_image(tag=f"layer0: {batch_ndx}", img_tensor=img_grid0)
+    writer.add_image(tag=f"layer1: {batch_ndx}", img_tensor=img_grid1)
+    writer.add_image(tag=f"layer2: {batch_ndx}", img_tensor=img_grid2)
+
     writer.close()
 
     exit()
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+# MNIST
 # import mnist.lenet5
 # import mnist.alex
 
+
+# Data Augmentation
 # import data_augmentation.a_dataset_normalizer
 # import data_augmentation.b_dataset_builder
 # import data_augmentation.c_dataset_visualization
 # import data_augmentation.transformation
 
-
+# Image Processing
 import image_processing.conv_pytorch
 # import image_processing.conv_opencv
 # import image_processing.semantic_segmentation
